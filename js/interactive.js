4439edd1c39820f7772988e93426e08b74d95ec7
dandres <d4ndres@gmail.com>
2018-08-02 21:57:39 -0400

    almost finished

--- js/interactive.js
+++ js/interactive.js
@@ -8,14 +8,62 @@
 // document.documentElement.scrollTop = $programs.offsetHeight
 
 const $btn = document.getElementById('btn')
-const $programs = document.getElementById('programs')
-const $header = document.getElementById('header')
+const $arrowUp = document.getElementById('arrow-up')
+const $arrowDown = document.getElementById('arrow-down')
+
+const $header = document.getElementById('mod1')
+const $programs = document.getElementById('mod2')
+const $mod3 = document.getElementById('mod3')
+const $footer = document.getElementById('mod4')
+
+const knowHeightMod = (elem) => elem.offsetHeight
+const DOCUMENTHEIGHT = 	[ knowHeightMod($header), knowHeightMod($programs), knowHeightMod($mod3), knowHeightMod($footer)] 
+
+
+//__________________________scrollbtns__________________
+// class FixedBtn {
+// 	constructor (btn, arr, state, positive) {
+// 		this.btn = btn
+// 		this.arr = arr
+// 		this.state = state
+// 		this.positive = positive
+// 		this.btn.addEventListener('click', () => this.onClcik())
+// 	}
+// 	onClcik () {
+// 		if ( this.positive ) {
+			
+// 			moverY(  this.arr[this.state++]  )
+// 			if (this.state == 3) this.state = 2
+// 			console.log('funciono? ')
+
+
+// 		} else if ( !this.positive) {
+
+// 			moverY( (-1 )* this.arr[this.state--]  )
+// 			if (this.state == 0) this.state = 1
+// 			console.log('funciono? eso espero ')
+// 		}
+// 	}
+// }
+// function findState(arr , initState = document.documentElement.scrollTop) {
+// 	let length = arr.length
+// 	let sum = 0
+// 	for (let i = 0; i < length; i++) {
+// 		sum += arr[i]
+// 		if (sum == initState ) return i
+// 	} 
+// }
+// function moverY(height = document.documentElement.scrollTop) {
+// 	console.log('todo bien')
+// 	document.documentElement.scrollTop += height
+// }
+// const arrowDown = new FixedBtn($arrowDown, DOCUMENTHEIGHT, findState(DOCUMENTHEIGHT), true )
+// const arrowUP = new FixedBtn($arrowUp, DOCUMENTHEIGHT, findState(DOCUMENTHEIGHT), false )
 
 // mejorar codigo, no creo que funcione para todads las pantallas
+// document.documentElement.scrollTop = $programs.offsetHeight
 $btn.addEventListener('click', () =>{
-	console.log('yupi')
 	let scroller = document.documentElement.scrollTop
-	// document.documentElement.scrollTop = $programs.offsetHeight
 	let scrollingPrograms = setInterval( () =>{
 		document.documentElement.scrollTop = scroller
 		scroller += 10
@@ -24,4 +72,8 @@ $btn.addEventListener('click', () =>{
 		 	clearInterval(scrollingPrograms)
 		}
 	},  1)
+
+	// document.documentElement.scrollTop = DOCUMENTHEIGHT[1]
+
+
 })
